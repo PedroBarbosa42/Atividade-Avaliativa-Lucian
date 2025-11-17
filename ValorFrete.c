da522cb4ec8aff91b38b4330977b66c848e6f000
Barbosa <pedrocombr93@gmail.com>
2025-11-17 00:16:31 +0000

    16/11/2025

---
+++ ValorFrete.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+
+    int regiao;
+
+    printf("Digite o numero correspondente ao seu estado para saber o valor do frete:\n\n1 - Sul\n2 - Sudeste\n3 - Centro-Oeste\n4 - Nordeste\n5 - Norte\n\n ");
+
+    scanf("%d", &regiao);
+
+    switch (regiao) {
+        case 1:
+            printf("O valor do frete para o Sul é de: R$5,00\n");
+            break; 
+
+        case 2:
+            printf("O valor do frete para o Suldeste é de: R$5,00\n");
+            break;
+
+        case 3:
+            printf("O valor do frete para o Centro-Oeste é de: R$7,50\n");
+            break;
+
+        case 4:
+            printf("O valor do frete para o Nordeste é de: R$12,50\n");
+            break;
+
+        case 5:
+            printf("O valor do frete para o Norte é de: R$15,00\n");
+            break;
+
+        default:
+            printf("Opcao invalida!\n");
+
+        }
+
+        return 0;
+}
