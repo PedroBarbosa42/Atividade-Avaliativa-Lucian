1e29c8e42a75c41067fd3463ad4145f736c0d3ef
Barbosa <pedrocombr93@gmail.com>
2025-11-17 13:40:31 +0000

    17/11/2025

--- Estacao.c
+++ Estacao.c
@@ -1,1 +1,47 @@
-#include "leitor.c"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    int estacao;
+
+    printf("--- Descubra a Estacão do Ano ---\n");
+    printf("Digite o número do mês\n 1 = Jan\n 2 = Fev\n 3 = Mar\n 4 = Abr\n 5 = Mai\n 6 = Jun\n 7 = Jul\n 8 = Ago\n 9 = Set\n 10 = Out\n 11 = Nov\n 12 = Dez\n");
+
+    scanf("%d", &estacao);
+
+    switch(estacao) {
+        case 1:
+        case 2:
+        case 12:
+            printf("A estação deste mês é: Verão\n");
+            break;
+
+        case 3:
+        case 4:
+        case 5:
+            printf("A estação deste mês é: Outono\n");
+            break;
+
+        case 6:
+        case 7: 
+        case 8:
+            printf("A estação deste mês é: Inverno\n");
+            break;
+
+        case 9:
+        case 10:
+        case 11:
+            printf("A estação deste mês é: Primavera\n");
+            break;
+
+        default:
+            printf("Número invalido!!");
+        
+        }
+
+        return 0;
+
+}
+
+
--- Menu.c
+++ Menu.c
@@ -1,1 +1,14 @@
-#include "leitor.c"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    int menu;
+
+    printf("Escolha um tipo de carne:\n ");
+    printf("1	Filé de frango	15,00\n 2
+Bisteca suína	15,00\n 3	Carne de panela	17,50 \n 4	Peixe empanado	16,00\n 5	Carne de soja	18,00\n");
+}
+
+
+
